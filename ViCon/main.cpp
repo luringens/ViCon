acba810ba90690fa4cdd689c6420b2ca1d7a3458
Stian Soltvedt <soltvedt.stian@gmail.com>
2017-02-24 17:06:43 +0100

    Split main into multiple functions

--- ViCon/main.cpp
+++ ViCon/main.cpp
@@ -15,6 +15,10 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error %d: %s\n", error, description);
 }
 
+bool SetupEscapi(SimpleCapParams&, GLuint&, int);
+void CreateDebugdataWindow(int, ImVec4&, int, float*, int);
+void CreateWebcamWindow(GLuint, int, int);
+
 int main(int, char**)
 {
     // Setup window
@@ -32,29 +36,10 @@ int main(int, char**)
     ImGui_ImplGlfwGL3_Init(window, true);
 
     // Setup ESCAPI
-	auto devices = setupESCAPI();
-	if (devices == 0)
-	{
-		std::cout << "ESCAPI initialization failure or no devices found." << std::endl;
-		return-1;
-	}
-	auto devicenr = 1;
-	struct SimpleCapParams capture;
-	capture.mWidth = 256;
-	capture.mHeight = 256;
-	capture.mTargetBuf = new int[256 * 256];
 	GLuint texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // Linear Filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Linear Filtering
-	if (initCapture(devicenr, &capture) == 0)
-	{
-		std::cout << "ESCAPI: The device may already be in use!" << std::endl;
-		return-1;
-	}
-	doCapture(devicenr);
-	while (isCaptureDone(devicenr) == 0) { /* Wait for first frame */ }
+	struct SimpleCapParams capture;
+	auto devicenr = 1;
+	SetupEscapi(capture, texture, devicenr);
 
 	// Setup GUI variables
 	ImVec4 clear_color = ImColor(114, 144, 154);
@@ -93,55 +78,31 @@ int main(int, char**)
 			framesumcount = 0;
 		}
 
+		// Get next webcam frame if ready
+		if (isCaptureDone(devicenr) == 1) {
+			// Swap red and blue to convert from BGRA to RGBA
+			for (auto i = 0; i < capture.mWidth * capture.mHeight; i++)
+				capture.mTargetBuf[i] = capture.mTargetBuf[i] & 0xff00ff00 |
+				(capture.mTargetBuf[i] & 0xff) << 16 |
+				(capture.mTargetBuf[i] & 0xff0000) >> 16 |
+				0xff000000; // And set alpha to 255 to make it opaque
+
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, capture.mWidth, capture.mHeight,
+				0, GL_RGBA, GL_UNSIGNED_BYTE, static_cast<GLvoid*>(capture.mTargetBuf));
+
+			doCapture(devicenr);
+		}
+
 		// Get frame size
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
 
         // Debug data window
-        {
-			ImGui::SetNextWindowPos(ImVec2(0, 0));
-			ImGui::SetNextWindowSize(ImVec2(400, display_h), ImGuiSetCond_Always);
-			ImGui::Begin("Debug data", nullptr, ImGuiWindowFlags_NoMove |
-											    ImGuiWindowFlags_NoResize |
-				                                ImGuiWindowFlags_NoCollapse |
-											    ImGuiWindowFlags_NoBringToFrontOnFocus);
-			
-            static auto f = 0.0f;
-            ImGui::Text("Hello, world!");
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-            ImGui::ColorEdit3("clear color", reinterpret_cast<float*>(&clear_color));
-            
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / framerate, framerate);
-			
-			ImGui::PlotHistogram("Frametimes", frametimes, frametimesCount, 0, 0, 
-				FLT_MAX, FLT_MAX, ImVec2(300, 50));
-			ImGui::End();
-		}
+		CreateDebugdataWindow(display_h, clear_color, framerate, frametimes, frametimesCount);
 
         // Webcam window
-        {
-            ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiSetCond_Always);
-            ImGui::Begin("Webcam live feed");
-            
-			if (isCaptureDone(devicenr) == 1) {
-				for (auto i = 0; i < 256 * 256; i++)
-					capture.mTargetBuf[i] = capture.mTargetBuf[i] & 0xff00ff00 |
-					(capture.mTargetBuf[i] & 0xff) << 16 |
-					(capture.mTargetBuf[i] & 0xff0000) >> 16 |
-					(0xff000000);
-
-				glBindTexture(GL_TEXTURE_2D, texture);
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, capture.mWidth, capture.mHeight,
-					0, GL_RGBA, GL_UNSIGNED_BYTE, static_cast<GLvoid*>(capture.mTargetBuf));
-
-				doCapture(devicenr);
-			}
-
-			ImVec2 picsize(capture.mWidth, capture.mHeight);
-			ImGui::Image(reinterpret_cast<ImTextureID>(texture), picsize);
-
-            ImGui::End();
-        }
+		CreateWebcamWindow(texture, capture.mWidth, capture.mHeight);
 
         // Rendering
         glViewport(0, 0, display_w, display_h);
@@ -158,4 +119,57 @@ int main(int, char**)
     glfwTerminate();
 
     return 0;
+}
+
+bool SetupEscapi(SimpleCapParams &capture, GLuint &texture, int devicenr)
+{
+	capture.mWidth = 500;
+	capture.mHeight = 500;
+	capture.mTargetBuf = new int[capture.mWidth * capture.mHeight];
+
+	auto devices = setupESCAPI();
+	if (devices == 0)
+	{
+		std::cout << "ESCAPI initialization failure or no devices found." << std::endl;
+		return false;
+	}
+	
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // Linear Filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Linear Filtering
+	
+	if (initCapture(devicenr, &capture) == 0) {
+		std::cout << "ESCAPI: The device may already be in use!" << std::endl;
+		return false;
+	}
+
+	doCapture(devicenr);
+	while (isCaptureDone(devicenr) == 0) { /* Wait for first frame */ }
+
+	return true;
+}
+
+void CreateDebugdataWindow(int windowHeight, ImVec4 &clear_color, int framerate, float* frametimes, int frametimesCount)
+{
+	ImGui::SetNextWindowPos(ImVec2(0, 0));
+	ImGui::SetNextWindowSize(ImVec2(400, windowHeight), ImGuiSetCond_Always);
+	ImGui::Begin("Debug data", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus |
+		ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+	static auto f = 0.0f;
+	ImGui::Text("Hello, world!");
+	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+	ImGui::ColorEdit3("clear color", reinterpret_cast<float*>(&clear_color));
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / framerate, framerate);
+	ImGui::PlotHistogram("Frametimes", frametimes, frametimesCount, 0,
+		nullptr, FLT_MAX, FLT_MAX, ImVec2(300, 50));
+	ImGui::End();
+}
+
+void CreateWebcamWindow(GLuint texture, int width, int height)
+{
+	ImGui::SetNextWindowSize(ImVec2(500, 530), ImGuiSetCond_Always);
+	ImGui::Begin("Webcam live feed");
+	ImGui::Image(reinterpret_cast<ImTextureID>(texture), ImVec2(width, height));
+	ImGui::End();
 }
